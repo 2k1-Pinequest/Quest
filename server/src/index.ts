d3a7993ff6819f4447413fb9380bdcfc26ef1a64
25LP2339 25LP2339 <25LP2339@admins-iMac.local>
2025-09-11 02:44:43 -0700

    1

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
