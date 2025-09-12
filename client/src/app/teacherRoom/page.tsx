ce88c610a78334f6cd436b5b0d27c9e3b4fd7faf
25LP3742 25LP3742 <zoljargal.g247@gmail.com>
2025-09-12 16:33:41 +0800

    teacherDashboard hiisen ok

--- client/src/app/teacherRoom/page.tsx
+++ client/src/app/teacherRoom/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import TeacherDashboard from "@/components/teacher/teacherDashboard";
 import UploadFile from "./uploadFile";
-
+import { Quiz } from "@/types/quiz";
 
 export default function Home() {
+  const handleBack = () => {};
+  const handleStartQuiz = (quiz: Quiz) => {};
   return (
     <div>
-      <UploadFile/>
+      <TeacherDashboard onBack={handleBack} onStartQuiz={handleStartQuiz} />
+      <UploadFile />
     </div>
   );
 }
