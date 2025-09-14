a3bf65b2c4ac8a4217022d1feac614ace708cd22
Bolormaa <boloroots47@gmail.com>
2025-09-14 16:29:36 +0800

    Add assignment detail structure and routing for teacher view

--- client/src/app/teacherAssignmentDetail/[assignId]/assignmentSubAnalyze.tsx
+++ client/src/app/teacherAssignmentDetail/[assignId]/assignmentSubAnalyze.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { TeacherAssignDetailHeader } from "@/components/teacher/assignmentDetail/assignmentdetailHeader";
 import { AssignmentTab } from "@/components/teacher/assignmentDetail/assignmentTab";
 
-export const AssignmentDetails = () => {
+export const AssignmentDetails = ({assignmentId}:{assignmentId:string}) => {
   return (
     <div className="min-h-screen p-6">
       <TeacherAssignDetailHeader />
