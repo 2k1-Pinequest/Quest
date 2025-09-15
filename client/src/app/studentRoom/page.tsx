2f48c5020cf43b4efc1ba3f4f507b692cd5d8500
25LP0882 25LP0882 <25LP0882@administrators-iMac.local>
2025-09-15 12:15:06 +0800

    student auth with join

--- client/src/app/studentRoom/page.tsx
+++ client/src/app/studentRoom/page.tsx
@@ -5,26 +5,47 @@ import { useRouter } from "next/navigation";
 import StudentSignup from "@/components/auth/student/signUp";
 import StudentLogin from "@/components/auth/student/signIn";
 import StudentDashboard from "@/components/student/StudentDashboard";
+import { StudentJoin } from "@/components/student/studentJoin";
 
 export default function Home() {
   const router = useRouter();
-  const [step, setStep] = useState<"signup" | "login" | "dashboard">("signup");
 
-  // Signup амжилттай бол login руу шилжүүлэх
+  const [studentId, setStudentId] = useState<number | null>(null);
+
+  const [step, setStep] = useState<
+    "login" | "signup" | "joinclass" | "dashboard"
+  >("login");
+
   const handleSignupSuccess = () => {
     setStep("login");
   };
 
-  // Login амжилттай бол dashboard руу шилжүүлэх
-  const handleLoginSuccess = () => {
+  const handleLoginSuccess = (studentId: number) => {
+    setStudentId(studentId);
+    setStep("joinclass");
+  };
+
+  const handleNext = (name: string, roomCode: string) => {
+    if (studentId === null) {
+      console.error("Student ID is null. Cannot join class.");
+      return;
+    }
+    console.log("Student ID:", studentId);
+
+    console.log("Student name:", name);
+    console.log("Room code:", roomCode);
     setStep("dashboard");
-    router.push("/studentRoom/dashboard"); // Dashboard руу redirect
+    // router.push("/studentRoom/dashboard");
+    router.push(`/studentRoom/dashboard?studentId=${studentId}`);
   };
 
   return (
     <div className="max-w-3xl mx-auto mt-10 space-y-6">
       {step === "signup" && <StudentSignup onSuccess={handleSignupSuccess} />}
       {step === "login" && <StudentLogin onSuccess={handleLoginSuccess} />}
+      {step === "joinclass" && studentId !== null && (
+        <StudentJoin onNext={handleNext} studentId={studentId} />
+      )}
       {step === "dashboard" && <StudentDashboard />}
     </div>
   );
