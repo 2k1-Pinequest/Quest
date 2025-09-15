6fd60fd9c06e72e997feb6d3ad3706aaba7c4684
25LP0882 25LP0882 <25LP0882@administrators-iMac.local>
2025-09-15 10:27:35 +0800

    router student added login

--- server/src/routes/student-routes.ts
+++ server/src/routes/student-routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { joinRoom } from "../controllers/room/joinRoom.controller";
 import { createStudent } from "../controllers/student/signUp.controller";
+import { loginStudent } from "../controllers/student/signIn.controller";
 
 const studentRouter = Router();
 
 studentRouter.post("/joinclass/:studentId", joinRoom);
 studentRouter.post("/register", createStudent);
+studentRouter.post("/login", loginStudent);
 export default studentRouter;
+//ene hseg deer shalgah
