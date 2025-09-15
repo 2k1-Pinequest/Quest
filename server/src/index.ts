23572f65c77e25805f4b338fb3e9fb95a960091d
Bolormaa <boloroots47@gmail.com>
2025-09-16 02:32:42 +0800

    gemini-api-first

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import roomRouter from "./routes/room-routes";
 import teacherRouter from "./routes/createTeacher";
 import studentRouter from "./routes/student-routes";
+import assignmentRouter from "./routes/StudentAssignment.router";
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use("/room", roomRouter);
 app.use("/teacher", teacherRouter);
 app.use("/student", studentRouter);
+
+app.use("/studentAssign", assignmentRouter)
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
