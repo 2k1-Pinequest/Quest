83835a7b0a82949defb4b67e8403dd4c7b4d8455
dudu <user@users-MacBook-Air.local>
2025-09-16 17:44:25 +0800

    create assignment

--- server/src/routes/createTeacher.ts
+++ server/src/routes/createTeacher.ts
@@ -2,14 +2,14 @@ import { Router } from "express";
 
 import { loginTeacher } from "../controllers/teacher/sign-in.controller";
 import { createTeacher } from "../controllers/teacher/sign-up.controller";
-import { createAssignment } from "../controllers/teacher/CreateAssigment.controller";
+import { createAssignmentForRoom } from "../controllers/teacher/CreateAssigment.controller";
+
 
 const teacherRouter = Router();
 
 teacherRouter.post("/sign-up", createTeacher);
 teacherRouter.post("/sign-in", loginTeacher);
-
-teacherRouter.post("/createAssignment", createAssignment)
+teacherRouter.post("/createAssignment", createAssignmentForRoom)
 
 
 
