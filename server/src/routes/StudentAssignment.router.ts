5d096e2af7bc5798c8d9cd03d168dec498072036
Bolormaa <boloroots47@gmail.com>
2025-09-23 13:10:05 +0800

    h

--- server/src/routes/StudentAssignment.router.ts
+++ server/src/routes/StudentAssignment.router.ts
@@ -1,18 +1,11 @@
+// assignmentRouter.ts
 import express from "express";
-import multer from "multer";
 import { analyzeAssignment } from "../controllers/GeminiStudent/studentSubmitAnalyze.controller";
 
 const assignmentRouter = express.Router();
 
-// ✅ Multer memoryStorage ашиглана (serverless-д тохиромжтой)
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 // POST endpoint – олон зураг upload хийж анализ хийх
-assignmentRouter.post(
-  "/:assignmentId/:studentId",
-  upload.array("files", 4),
-  analyzeAssignment
-);
+assignmentRouter.post("/:assignmentId/:studentId", analyzeAssignment);
 
 export default assignmentRouter;
+
